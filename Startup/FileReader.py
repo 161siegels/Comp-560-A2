e86a120d572b810242f555c38e3e3a5e2d60bd57
James Bury <jamesbury77@gmail.com>
2020-04-08 14:24:28 -0400

    Removed commented out code

--- Startup/FileReader.py
+++ Startup/FileReader.py
@@ -11,11 +11,11 @@ def read_from_file(file_name: str):
     ]
 
     for line in file:
+        
         shot_types_iterator = iter(shot_types)
         new_shot = {}
 
-        current_line = line.rstrip()
-        sections = current_line.split(sep="/")
+        sections = line.rstrip().split(sep="/")
 
         for element in sections:
             new_shot[next(shot_types_iterator)] = element
@@ -23,17 +23,3 @@ def read_from_file(file_name: str):
         shots.append(Shot(new_shot))
 
     return shots
-    # input_type = list(inputs.keys())
-    # type_iterator = iter(input_type)
-    # current = next(type_iterator)
-    #
-    # for line in file:
-    #
-    #     if line.rstrip() == '':
-    #         current = next(type_iterator)
-    #         continue
-    #
-    #     inputs[current].append(line.rstrip())
-    #
-    # file.close()
-    # return inputs
